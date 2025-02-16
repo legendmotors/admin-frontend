772a25dc17c77d9ffb489d7661717f36cfacc69a
mohamedsahin <mohamedsahinva@gmail.com>
2025-02-16 23:18:19 +0530

    0

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
+    assetPrefix: process.env.NODE_ENV === 'production' ? 'https://admin.legendmotorsuae.com' : '',
+    basePath: process.env.NODE_ENV === 'production' ? '' : '',
 };
 
 module.exports = nextConfig;
